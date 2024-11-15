57a8126f38ecfb206d0735a5a6be9ee3776d0a3f
NimsayS <yasmin.d.lim@gmail.com>
2024-11-15 14:34:39 -0300

    :raised_hands:

---
+++ atividade05/app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());
+
+
+let animes = [];
+
+
+
+module.exports = app;
+
+const { v4: uuidv4 } = require("uuid");
+
+app.post("/animes", (req, res) => {
+  const { name, genre, studio } = req.body;
+
+  if (!name || !genre || !studio) {
+    return res.status(400).json({ error: "Os campos são obrigatórios" });
+  }
+
+  const newAnime = {
+    id: uuidv4(), 
+    name,
+    genre,
+    studio,
+  };
+
+  animes.push(newAnime);
+  res.status(201).json(newAnime);
+});
+
+  app.get("/animes", (req, res) => {
+    res.json(animes);
+  });
+
+  app.get("/animes/:id", (req, res) => {
+    const { id } = req.params; 
+    const anime = animes.find((a) => a.id === id); 
+  
+    if (!anime) {
+      return res.status(404).json({ error: "anime não encontrado" });
+    }
+  
+    res.json(anime);
+  });
+
+  app.put("/animes/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, genre, studio } = req.body;
+  
+    const anime = animes.find((a) => a.id === id); 
+  
+    if (!anime) {
+      return res.status(404).json({ error: "anime não encontrado" });
+    }
+  
+
+    if (!name || !genre || !studio) {
+      return res.status(400).json({ error: "Os campos são obrigatórios" });
+    }
+  
+
+    anime.name = name;
+    anime.genre = genre;
+    anime.studio = studio;
+  
+    res.json(anime);
+  });
+
+  app.delete("/animes/:id", (req, res) => {
+    const { id } = req.params;
+    const index = animes.find((a) => a.id === id); 
+  
+    if (index === -1) {
+      return res.status(404).json({ error: "anime não encontrado" });
+    }
+  
+    animes.splice(index, 1);
+    res.status(200).send(); 
+  });
